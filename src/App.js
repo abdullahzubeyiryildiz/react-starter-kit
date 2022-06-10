b0176b05c013cfc0f79fd661382d37d45ee003e2
Abdullah Zübeyir Yıldız <zubeyir_55@hotmail.com>
2022-06-10 10:22:57 +0300

    React Başlangıç

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <img src="/logo192.png" /> 
+    <img src={logo} /> 
+    </div>
+  );
+}
+
+export default App;
